b5607bd6d1eff69196bee6c83bb873fc6db2aea4
Ngai Lam Chou <ngai.lam.chou@gmail.com>
2024-04-07 23:30:29 -0700

    better format

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 var url;
+var weatherData;
+
+async function fetchData() {
+  const response = await fetch(url);
+  weatherData = await response.json();
+}
 
 async function getTemp() {
-  const response = await fetch(url);
-  const data = await response.json();
-  document.getElementById(
-    "temp"
-  ).innerText = `Max Temperature : ${JSON.stringify(
-    Math.round(((data.days[0].tempmax - 32) * 5) / 9)
-  )}°C\n
-    Current Temperature : ${JSON.stringify(
-      Math.round(((data.days[0].temp - 32) * 5) / 9)
-    )}°C\n
-    Min Temperature : ${JSON.stringify(
-      Math.round(((data.days[0].tempmin - 32) * 5) / 9)
-    )}°C\n`;
+  document.getElementById("curr-temp").innerText = `${JSON.stringify(Math.round(((weatherData.days[0].temp - 32) * 5) / 9))}°\n`
+  document.getElementById("temp").innerText = `H:${JSON.stringify(Math.round(((weatherData.days[0].tempmax - 32) * 5) / 9)
+  )}° L:${JSON.stringify(
+      Math.round(((weatherData.days[0].tempmin - 32) * 5) / 9)
+    )}°\n`;
 }
 
 async function getCondition() {
-  const response = await fetch(url);
-  const data = await response.json();
   document.getElementById("conditions").innerText =
-    data.days[0].conditions + "\n";
-  document.querySelector(
-    "img"
-  ).src = `condition_images/${data.days[0].icon}.png`;
+    weatherData.days[0].conditions.split(",")[0]
+    .trim() + "\n";
 }
 
 async function getLocation() {
-  const response = await fetch(url);
-  const data = await response.json();
-  document.querySelector("h1").innerText =
-    `${data.address}`.charAt(0).toUpperCase() + `${data.address}`.slice(1);
+  document.querySelectorAll("h1")[1].innerText = `${weatherData.resolvedAddress
+    .split(",")[0]
+    .trim()}`;
   document.querySelector(
     "body"
-  ).style.backgroundImage = `url(image/${data.address}.jpg)`;
+  ).style.backgroundImage = `url(image/${weatherData.resolvedAddress
+    .split(",")[0]
+    .trim()}.jpg)`;
 }
 
 async function changeLocation(address) {
   try {
     url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${address}?unitGroup=us&include=current&key=WVGNQMSX3PTP72YYZETFA7NB7&contentType=json`;
+    await fetchData();
     await getTemp();
     await getCondition();
     await getLocation();
   } catch (error) {
-    document.querySelectorAll("h1")[1].innerText = "Exceed API Use Limit\nCome Again Tommorow";
+    document.querySelectorAll("h1")[1].innerText =
+      "Exceed API Use Limit\nCome Again Tomorrow";
   }
 }
 
